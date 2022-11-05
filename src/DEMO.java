a72e665ea8d67d5e760db1b48319470ce3e7b1e0
Prathamesh <prathameshdhakane89@gmail.com>
2022-11-06 00:38:32 +0530

    pushing code 2nd time

---
+++ src/DEMO.java
@@ -0,0 +1,8 @@
+import java.time.Clock;
+
+public class DEMO {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stu
+        System.out.println("TESTDEMO");
+    }
+}
